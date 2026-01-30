f7d009af42c9b64eb6c3d7bc1c7d48a5fd990e13
kenneyher <kenneyher@gmail.com>
2026-01-30 15:15:40 -0600

    complete phase 2: handling hardware constraints

--- App.js
+++ App.js
@@ -1,40 +1,71 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
-import { use, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { use, useState } from "react";
 
 export default function App() {
-  const [ tasks, setTasks ] = useState([]);
-  const [ task, setTask ] = useState(''); 
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
 
   const addTask = () => {
     setTasks([...tasks, task]);
-    setTask('');
-  }
+    setTask("");
+  };
 
   return (
-    <View style={{ flex: 1, paddingTop: 50 }}>
-      <Text style={{ fontSize: 30, textAlign: 'center' }}>TO-DO List</Text>
-      <ScrollView>
-        {tasks.map((t, index) => (
-          <Text key={index} style={{ fontSize: 20, padding: 10 }}>{t}</Text>
-        ))}
-      </ScrollView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <Text style={{ fontSize: 30, textAlign: "center" }}>TO-DO List</Text>
+          <View style={{ flex: 1}}>
+            {tasks.map((t, index) => (
+              <Text key={index} style={{ fontSize: 20, padding: 10 }}>
+                {t}
+              </Text>
+            ))}
+          </View>
 
-      <TextInput 
-        placeholder='New Task'
-        value={task}
-        onChangeText={setTask}
-        style={{ borderWidth: 1, margin: 20, padding: 10}}
-      />
-      <Button title="Add" onPress={addTask} />
-    </View>
+          <TextInput
+            style={styles.input}
+            placeholder="New Task"
+            value={task}
+            onChangeText={setTask}
+            onEndEditing={() => {
+              setTasks([...tasks, task]);
+              setTask("");
+            }}
+          />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
   },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: "#d3d3d3",
+    width: "90%",
+    fontSize: 16,
+    height: 50, 
+    padding: 10,
+    borderRadius: 8,
+  }
 });
